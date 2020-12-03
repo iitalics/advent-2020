ac17f50ba7fbd04acd65d25342694be9805dd89a
Milo Turner <iitalics@gmail.com>
2020-12-03 17:21:12 -0500

    8-space tabs

--- day1.c
+++ day1.c
@@ -5,23 +5,23 @@ static char S[N + 1] = {0};
 
 int main()
 {
-    int i, j, k;
+        int i, j, k;
 
-    while (scanf("%d\n", &i) > 0) {
-        if (S[N - i]) {
-            printf("%d", i * (N - i));
+        while (scanf("%d\n", &i) > 0) {
+                if (S[N - i]) {
+                        printf("%d", i * (N - i));
+                }
+
+                for (j = 0; j <= N - i; j++) {
+                        k = N - i - j;
+                        if (S[j] && S[k]) {
+                                printf(", %d\n", i * j * k);
+                                break;
+                        }
+                }
+
+                S[i] = 1;
         }
 
-        for (j = 0; j <= N - i; j++) {
-            k = N - i - j;
-            if (S[j] && S[k]) {
-                printf(", %d\n", i * j * k);
-                break;
-            }
-        }
-
-        S[i] = 1;
-    }
-
-    return 0;
+        return 0;
 }
--- day3.c
+++ day3.c
@@ -2,36 +2,36 @@
 
 int main()
 {
-    int x, y, w, even;
-    long long int t11, t31, t51, t71, t12;
-    char ch;
+        int x, y, w, even;
+        long long int t11, t31, t51, t71, t12;
+        char ch;
 
-    y = x = 0;
-    even = w = 1;
-    t11 = t31 = t51 = t71 = t12 = 0;
+        y = x = 0;
+        even = w = 1;
+        t11 = t31 = t51 = t71 = t12 = 0;
 
-    while ((ch = getchar()) != EOF) {
-        switch (ch) {
-        case '\n':
-            w = x;
-            x = 0;
-            y++;
-            even = !even;
-            break;
+        while ((ch = getchar()) != EOF) {
+                switch (ch) {
+                case '\n':
+                        w = x;
+                        x = 0;
+                        y++;
+                        even = !even;
+                        break;
 
-        case '#':
-            if (x == y       % w) t11++;
-            if (x == (y * 3) % w) t31++;
-            if (x == (y * 5) % w) t51++;
-            if (x == (y * 7) % w) t71++;
-            if (x == (y / 2) % w && even) t12++;
+                case '#':
+                        t11 += (x == y       % w);
+                        t31 += (x == (y * 3) % w);
+                        t51 += (x == (y * 5) % w);
+                        t71 += (x == (y * 7) % w);
+                        t12 += (x == (y / 2) % w && even);
 
-        default:
-            x++;
-            break;
+                default:
+                        x++;
+                        break;
+                }
         }
-    }
 
-    printf("%lld, %lld\n", t31, (t11 * t31 * t51 * t71 * t12));
-    return 0;
+        printf("%lld, %lld\n", t31, (t11 * t31 * t51 * t71 * t12));
+        return 0;
 }
