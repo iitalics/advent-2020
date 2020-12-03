cbd450167a2380fac251246f46e0894520e159a5
Milo Turner <iitalics@gmail.com>
2020-12-03 16:57:10 -0500

    day 2 part 1/2

---
+++ day2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int lo, hi, i, n, m, valid1, valid2;
+    char ch, reqch;
+
+    valid1 = valid2 = 0;
+    while (scanf("%d-%d %c: ", &lo, &hi, &reqch) != -1) {
+
+        n = m = 0;
+        for (i = 1; (ch = getchar()) != '\n'; i++) {
+            if (ch != reqch) {
+                continue;
+            }
+            n++;
+            if (i == lo || i == hi) {
+                m++;
+            }
+        }
+
+        valid1 += (n >= lo && n <= hi);
+        valid2 += (m == 1);
+    }
+
+    printf("%d, %d\n", valid1, valid2);
+    return 0;
+}
